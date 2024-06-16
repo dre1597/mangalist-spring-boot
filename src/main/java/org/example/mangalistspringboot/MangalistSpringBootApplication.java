b066634d3c9cb2da8548b12759533fefc14919a9
André Pereira <andrepereira97@yahoo.com>
2024-06-16 00:52:27 -0300

    :tada: start project

---
+++ src/main/java/org/example/mangalistspringboot/MangalistSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.mangalistspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangalistSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MangalistSpringBootApplication.class, args);
+  }
+
+}
