224c6e498e0c9bacef3b0f993157f11c807e7735
André Pereira <andrepereira97@yahoo.com>
2025-02-16 18:12:13 -0300

    :white_check_mark: add custom exception handler test

---
+++ src/test/java/org/example/mangalistspringboot/infra/api/exceptions/CustomExceptionHandlerTest.java
@@ -0,0 +1,83 @@
+package org.example.mangalistspringboot.infra.api.exceptions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CustomExceptionHandlerTest {
+  @InjectMocks
+  private CustomExceptionHandler customExceptionHandler;
+
+  private MethodArgumentNotValidException methodArgumentNotValidException;
+  private BindingResult bindingResult;
+
+  @BeforeEach
+  void setUp() {
+    bindingResult = mock(BindingResult.class);
+    methodArgumentNotValidException = new MethodArgumentNotValidException(null, bindingResult);
+  }
+
+  @Test
+  void shouldHandleMethodArgumentNotValidException() {
+    var fieldError = new FieldError("objectName", "fieldName", "defaultMessage");
+    when(bindingResult.getFieldErrors()).thenReturn(Collections.singletonList(fieldError));
+
+    var response = customExceptionHandler.handle(methodArgumentNotValidException);
+
+    assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
+    assertEquals("Validation failed", response.getBody().get("message"));
+
+    var errors = (Map<String, String>) response.getBody().get("errors");
+    assertEquals("defaultMessage", errors.get("fieldName"));
+  }
+
+  @Test
+  void shouldHandleHttpMessageNotReadableException() {
+    var exception = new HttpMessageNotReadableException("Invalid JSON");
+
+    var response = customExceptionHandler.handleHttpMessageNotReadableException(exception);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(400, response.getBody().get("statusCode"));
+    assertEquals("Invalid request body. Please check the input values.", response.getBody().get("message"));
+    assertEquals("Invalid JSON", response.getBody().get("details"));
+  }
+
+  @Test
+  void shouldHandleIllegalArgumentException() {
+    var exception = new IllegalArgumentException("Invalid argument");
+
+    var response = customExceptionHandler.handleIllegalArgumentException(exception);
+
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals(400, response.getBody().get("statusCode"));
+    assertEquals("Invalid argument", response.getBody().get("message"));
+  }
+
+  @Test
+  void shouldHandleNoSuchElementException() {
+    var exception = new NoSuchElementException("Resource not found");
+
+    var response = customExceptionHandler.handleNoSuchElementException(exception);
+
+    assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    assertEquals(404, response.getBody().get("statusCode"));
+    assertEquals("Resource not found", response.getBody().get("message"));
+  }
+}
